709fb9388a473b8e821517741b4b3997c7cb3a41
darshan45672 <drshnbhandary@gmail.com>
2024-12-29 15:35:20 +0530

    added widget to user list

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Exports\UsersExport;
 use App\Filament\Resources\UserResource;
+use App\Filament\Widgets\UserChartWidget;
 use App\Imports\UsersImport;
 use App\Models\User;
 use Filament\Actions\CreateAction;
@@ -23,6 +24,13 @@ class ListUsers extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+    
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            UserChartWidget::class,
         ];
     }
 }
