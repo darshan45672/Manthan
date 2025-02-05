1a1b4aaf23ba7d44d86bac7e1d667aab3785f6cc
darshan45672 <drshnbhandary@gmail.com>
2025-02-05 10:04:50 +0530

    implemented caching in links imporving app performance

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,47 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        {{-- <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
+    {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
 
-        <head>
-            <meta charset="utf-8">
-            <meta http-equiv="x-ua-compatible" content="ie=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <meta name="description" content="">
-        
-            <title>Manthan - AICTE Activty/Event management system</title>
-        
-            <link rel="shortcut icon" type="image/x-icon" href="{{ asset('ui/assets/img/favicon.png') }}">
-        
-            <!-- CSS here -->
-            <link rel="stylesheet" href="{{ asset('ui/assets/css/bootstrap.min.css') }}">
-            <link rel="stylesheet" href="{{ asset('ui/assets/css/fontawesome.min.css') }}">
-            <link rel="stylesheet" href="{{ asset('ui/assets/css/venobox.min.css') }}">
-            <link rel="stylesheet" href="{{ asset('ui/assets/css/animate.min.css') }}">
-            <link rel="stylesheet" href="{{ asset('ui/assets/css/keyframe-animation.css') }}">
-            <link rel="stylesheet" href="{{ asset('ui/assets/css/odometer.min.css') }}">
-            <link rel="stylesheet" href="{{ asset('ui/assets/css/nice-select.css') }}">
-            <link rel="stylesheet" href="{{ asset('ui/assets/css/daterangepicker.css') }}">
-            <link rel="stylesheet" href="{{ asset('ui/assets/css/swiper.min.css') }}">
-            <link rel="stylesheet" href="{{ asset('ui/assets/css/main.css') }}">
-        </head>
+    @php
+        $header = Cache::rememberForever('cached_header', function () {
+            return view('layouts.cached.cacheHeader')->render();
+        });
+    @endphp
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        @include('layouts.navigation')
+    {!! $header !!}
 
-        @yield('content')
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-        @include('layouts.footer')
-    </body>
+<body>
+    @include('layouts.navigation')
+
+    @yield('content')
+
+    @include('layouts.footer')
+</body>
+
+<!-- JS here -->
+@php
+    $footer = Cache::rememberForever('cached_footer', function () {
+        return view('layouts.cached.cacheJs')->render();
+    });
+@endphp
+
+{!! $footer !!}
+
 </html>
---
+++ resources/views/layouts/cached/cacheHeader.blade.php
@@ -0,0 +1,25 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+
+    <title>Manthan - AICTE Activty/Event management system</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('ui/assets/img/favicon.png') }}">
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{ asset('ui/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('ui/assets/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('ui/assets/css/venobox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('ui/assets/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('ui/assets/css/keyframe-animation.css') }}">
+    <link rel="stylesheet" href="{{ asset('ui/assets/css/odometer.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('ui/assets/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('ui/assets/css/daterangepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('ui/assets/css/swiper.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('ui/assets/css/main.css') }}">
+</head>
